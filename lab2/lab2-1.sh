2072a5e7836048ee757f47ff5ba594fcf43f11aa
maanti <m.antipov@outlook.com>
2018-12-01 01:16:04 +0300

    lab2-1.sh works not only with current dir

--- lab2/lab2-1.sh
+++ lab2/lab2-1.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
-# like this for simplicity, but prefer one-line
-#IFS="
-#";
-
 IFS=$'\n';
-
 # $1: dirName where to search for subdirs
 function getSubs {
-
 	gfind "$1" -maxdepth 1 -type d -printf "%C@ %p\n" | sort | awk -F/ ' /.\//{print $2}';
-	
 }
 
-for dir in $(getSubs .)
+subdirs=$(getSubs "$1");
+cd -- "$1";
+for dir in $subdirs
 do
 	read -ra subs <<< "$(getSubs "$dir")";
 	if [ ${#subs[@]} -ne 0 ]; then
--- lab2/lab2-2.sh
+++ lab2/lab2-2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-getent group |  nawk -v len="$1" -F":" 'split($4, arr, ",") == len {print $1}';
+
+# find groups with more than $1 users
+getent group |  nawk -v len="$1" -F: 'split($4, arr, ",") > len {print $1}';
