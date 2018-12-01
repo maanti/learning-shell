2c2277cb91b8248795a81a99b20b5a8df82bd334
maanti <m.antipov@outlook.com>
2018-12-01 09:57:36 +0300

    function getSubsWithMtime

--- lab2/lab2-1.sh
+++ lab2/lab2-1.sh
@@ -2,15 +2,23 @@
 IFS=$'\n';
 # $1: dirName where to search for subdirs
 function getSubs {
-	gfind "$1" -maxdepth 1 -type d -printf "%C@ %p\n" | sort | awk -F/ ' /.\//{print $2}';
+	gfind "$1" -maxdepth 1 -type d -printf "%C@ %p\n" | sort -r | awk -F/ ' /.\//{print $2}';
 }
 
-subdirs=$(getSubs "$1");
+# $1: dirName where to search for subdirs
+function getSubsWithMtime {
+	gfind "$1" -maxdepth 1 -type d -printf "%C@ %p\n" | sort -r;
+}
+
+subdirs=$(getSubsWithMtime "$1");
 cd -- "$1";
-for dir in $subdirs
+for dirWithMtime in $subdirs
 do
-	read -ra subs <<< "$(getSubs "$dir")";
-	if [ ${#subs[@]} -ne 0 ]; then
-		echo "$dir"
-	fi
+	dir=$(echo "$dirWithMtime" | awk -F/ ' /.\//{print $2}');
+	if [ "$dir" != '' ]; then
+		read -ra subs <<< "$(getSubs "$dir")";
+		if [ ${#subs[@]} -ne 0 ]; then
+			echo "$dirWithMtime";
+		fi
+	fi;
 done
